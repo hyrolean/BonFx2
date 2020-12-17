859700e39c2c85b0755427166120556840280d9e
hyrolean <74116278+hyrolean@users.noreply.github.com>
2020-12-17 21:03:59 +0900

    Fixed recogning failure of compiler error code.

--- build.bat
+++ build.bat
@@ -3,17 +3,16 @@ if exist "%VSVARS32_2008%" (
   call "%VSVARS32_2008%"
   if "%1" == "Clean" (
     echo -*-Clean-*-
-    VCBuild /clean /nocolor %2_VS2008.sln
+    VCBuild /clean /nocolor "%2_VS2008.sln"
   )else if "%1" == "Rebuild" (
     echo -*-Rebuild-*-
-    VCBuild /rebuild /nocolor %2_VS2008.sln
+    VCBuild /rebuild /nocolor "%2_VS2008.sln"
   )else if "%1" == "Build" (
     echo -*-Build-*-
-    VCBuild /nocolor %2_VS2008.sln
+    VCBuild /nocolor "%2_VS2008.sln"
   )else exit 1
 )else if exist "%VSMSBUILDCMD%" (
   call "%VSMSBUILDCMD%"
-  MSBuild /t:%1 /p:Configuration=Release /p:platform=Win32 %2.sln
-  if not "%ERRORLEVEL%"  == "0" exit %ERRORLEVEL%
-  MSBuild /t:%1 /p:Configuration=Release /p:platform=x64 %2.sln
+  MSBuild /t:%1 /p:Configuration=Release /p:platform=Win32 "%2.sln" || exit /k 1
+  MSBuild /t:%1 /p:Configuration=Release /p:platform=x64 "%2.sln"
 )else exit 1
